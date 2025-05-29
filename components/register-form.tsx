f3eb78dc4247b29c20dc42f8a6822211f8bb3b8f
KevinFourland <kevin.jefrifourland@gmail.com>
2025-05-29 19:34:13 +0700

    auth

---
+++ components/register-form.tsx
@@ -0,0 +1,91 @@
+"use client";
+import { FormEvent, useRef, useState } from "react";
+import { useRouter } from "next/navigation";
+import Link from "next/link";
+import { register } from "@/actions/register";
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+export function RegisterForm({
+    className,
+    ...props
+}: React.ComponentProps<"div">) {
+    const [error, setError] = useState<string>();
+    const router = useRouter();
+    const [loading, setLoading] = useState(false);
+
+    const ref = useRef<HTMLFormElement>(null);
+
+    const handleSubmit = async (formData: FormData) => {
+        const r = await register({
+            email: formData.get("email"),
+            password: formData.get("password"),
+            name: formData.get("name")
+        });
+        ref.current?.reset();
+        if (r?.error) {
+            setError(r.error);
+            return;
+        } else {
+            return router.push("/login");
+        }
+    };
+    return (
+        <div className={cn("flex flex-col gap-6", className)} {...props}>
+            <Card className="overflow-hidden p-0">
+                <CardContent className="grid p-0 md:grid-cols-2">
+                    <form className="p-6 md:p-8" ref={ref}
+                        action={handleSubmit}>
+                        <div className="flex flex-col gap-6">
+                            <div className="flex flex-col items-center text-center">
+                                <h1 className="text-2xl font-bold">Sign Up</h1>
+                            </div>
+                            {error && (
+                                <div className="text-red-500 text-sm" aria-live="polite">
+                                    {error}
+                                </div>
+                            )}
+                            <div className="grid gap-3">
+                                <Label htmlFor="name">Name</Label>
+                                <Input id="name" name="name" type="text" required />
+                            </div>
+                            <div className="grid gap-3">
+                                <Label htmlFor="email">Email</Label>
+                                <Input
+                                    id="email"
+                                    name="email"
+                                    type="email"
+                                    placeholder="m@example.com"
+                                    required
+                                />
+                            </div>
+                            <div className="grid gap-3">
+                                <Label htmlFor="password">Password</Label>
+                                <Input id="password" name="password" type="password" required />
+                            </div>
+                            <Button type="submit" className="w-full" disabled={loading}>
+                                {loading ? "Registering..." : "Register"}
+                            </Button>
+                            <div className="text-center text-sm">
+                                Already have an account?{" "}
+                                <Link href="/login" className="underline underline-offset-4">
+                                    Sign in
+                                </Link>
+                            </div>
+                        </div>
+                    </form>
+                    <div className="bg-muted relative hidden md:block">
+                        <img
+                            src="/placeholder.svg"
+                            alt="Image"
+                            className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+                        />
+                    </div>
+                </CardContent>
+            </Card>
+        </div>
+    );
+}
